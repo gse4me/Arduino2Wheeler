a2e9a2f942592a2b8718f31fa261f82d25b1b3ac
gse4me <vsandule@gmail.com>
2017-12-21 09:43:09 +0200

    Removing useless files

--- ArduGirobot_AtmelStudio/ArduGirobot_AtmelStudio/ArduinoCore/Debug/src/core/WMath.d
+++
@@ -1,7 +0,0 @@
-src/core/WMath.d src/core/WMath.o: ../src/core/WMath.cpp \
- e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
-
-e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
--- ArduGirobot_AtmelStudio/ArduGirobot_AtmelStudio/ArduinoCore/Debug/src/core/new.d
+++
@@ -1,7 +0,0 @@
-src/core/new.d src/core/new.o: ../src/core/new.cpp \
- e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
-
-e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-e:\ides\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
